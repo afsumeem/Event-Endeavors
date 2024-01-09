cb453bfd2c2cfe3f7a063e683adeb79ad6b4ce94
Afsana Meem <afsumeem@gmail.com>
2024-01-09 15:17:53 +0600

    ticket design done

--- src/components/UI/Ticket.tsx
+++ src/components/UI/Ticket.tsx
@@ -1,17 +1,50 @@
+import { Image } from "@nextui-org/react";
 import React from "react";
 import QRCode from "react-qr-code";
+import { CiLocationOn } from "react-icons/ci";
+import { FiClock } from "react-icons/fi";
 
 const Ticket = () => {
   const value = "afsana";
   return (
-    <div>
-      <h2>Print this page</h2>
-      <QRCode
-        size={256}
-        style={{ height: "auto", maxWidth: "100px", width: "100px" }}
-        value={value}
-        viewBox={`0 0 256 256`}
-      />
+    <div className="my-10">
+      <div className="flex justify-between items-center bg-[#fabfb6] p-8 border-dashed border-1 border-[#fd614a] ticketContainer">
+        <div className=" px-4 mb-8 lg:mb-0">
+          <Image
+            className=" rounded-lg shadow-lg h-60 w-72"
+            src="https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b"
+            alt="Concert Image"
+          />
+        </div>
+        <div className="">
+          <h1 className="text-xl text-white uppercase font-bold mb-4">
+            John Smith Live in Concert
+          </h1>
+
+          <div className="mb-6 text-white ">
+            <p className="text-xl  mb-2">
+              Id: #<span>afsaana</span>
+            </p>
+          </div>
+
+          <p className="text-sm text-white flex gap-1 mb-2 mt-10">
+            <FiClock className="text-xl text-white" />
+            Friday, April 15th at 8:00 PM
+          </p>
+          <p className="text-sm text-white flex gap-1 ">
+            <CiLocationOn className="text-xl text-white" />
+            1805 Geary Blvd, San Francisco, CA
+          </p>
+        </div>
+        <div className=" ">
+          <QRCode
+            style={{ height: "100px", width: "100px" }}
+            size={256}
+            value={value}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/eventTicket.tsx
+++ src/pages/eventTicket.tsx
@@ -1,20 +1,31 @@
+import RootLayout from "@/components/Layouts/RootLayout";
 import Ticket from "@/components/UI/Ticket";
+import { Button } from "@nextui-org/react";
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 
-const EventTicket: React.FC = () => {
+const EventTicket = () => {
   const componentRef = useRef<any>();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
   return (
-    <div>
+    <div className=" mt-10 ">
+      <Button
+        onClick={handlePrint}
+        className="text-white bg-[#fd614a] rounded-none block m-auto"
+      >
+        Download Ticket
+      </Button>
       <div ref={componentRef}>
         <Ticket />
       </div>
-      <button onClick={handlePrint}>Print this out!</button>
     </div>
   );
 };
 
 export default EventTicket;
+
+EventTicket.getLayout = function getLayout(page: React.ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
+};
