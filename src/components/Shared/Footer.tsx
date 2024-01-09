51ec50d33d8fbb00542a3afa0f3118ccf1e75a91
Afsana Meem <afsumeem@gmail.com>
2024-01-09 16:45:14 +0600

    footer done

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,75 +1,122 @@
-import Link from "next/link";
-import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGooglePlusG } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
 
+//
 const Footer = () => {
   return (
-    <div
-      className="min-h-80 mt-4 md:mt-20 px-2 md:px-10 pt-5 md:pt-10"
-      style={{ backgroundColor: "black" }}
-    >
-      <div className="grid grid-cols-12">
-        <div className="col-span-4">
-          <div className="px-5 pt-4 text-white">
-            <h5 className="text-uppercase mb-3 text-2xl font-bold font-mono">
-              Event Endeavors
-            </h5>
-            <p className="text-base mb-8 text-justify">
-              Explore a diverse range of events curated just for you. From
-              captivating cultural festivals to heart-pounding live
-              performances, we have got something for every taste and interest.
-              Our events promise to captivate your senses and leave you craving
-              for more.
-            </p>
+    <div className="pt-10 mt-10">
+      {/* <!-- Footer container --> */}
+      <footer className="bg-[#ffbab0] text-center text-black dark:bg-neutral-600 lg:text-left">
+        <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between bg-[#ff9382]">
+          <div className="mr-12 hidden lg:block font-bold">
+            <span>Get connected with us on social networks:</span>
+          </div>
+          {/* <!-- Social network icons container --> */}
+          <div className="flex justify-center ">
+            <a href="#!" className="mr-6 text-black text-xl">
+              <FaFacebookF />
+            </a>
+            <a href="#!" className="mr-6 text-black text-xl">
+              <FaTwitter />
+            </a>
+            <a href="#!" className="mr-6 text-black text-xl">
+              <FaGooglePlusG />
+            </a>
+            <a href="#!" className="mr-6 text-black text-xl">
+              <FaInstagram />
+            </a>
+            <a href="#!" className="mr-6 text-black text-xl">
+              <FaLinkedinIn />
+            </a>
           </div>
         </div>
 
-        <div className="px-5 pt-4 text-start md:text-center text-white col-span-4">
-          <h5 className="text-uppercase mb-3 text-2xl font-bold font-mono">
-            Quick Links
-          </h5>
-          <Link href="/services">
-            <button className="text-white text-base underline">Services</button>
-          </Link>
-          <br />
-
-          <Link href="/projects">
-            <button className="text-white text-base underline">Projects</button>
-          </Link>
-          <br />
-
-          <button className="text-white text-base underline">Logout</button>
+        <div className="mx-6 py-10 text-center md:text-left">
+          <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+            {/* <!-- TW elements section --> */}
+            <div className="">
+              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start text-[#fd614a] text-xl">
+                Event Endeavors
+              </h6>
+              <p className="text-justify">
+                Explore a diverse range of events curated just for you. From
+                captivating cultural festivals to heart-pounding live
+                performances, we have got something for every taste and
+                interest.
+              </p>
+            </div>
+            {/* <!-- Products section --> */}
+            <div className="">
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start text-[#fd614a]">
+                Categories
+              </h6>
+              <p className="mb-4">
+                <a href="#!">Tech Conference</a>
+              </p>
+              <p className="mb-4">
+                <a href="#!">Music Concert</a>
+              </p>
+              <p className="mb-4">
+                <a href="#!">Food Festival</a>
+              </p>
+              <p>
+                <a href="#!">Sports Event</a>
+              </p>
+            </div>
+            {/* <!-- Useful links section --> */}
+            <div className="">
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start text-[#fd614a]">
+                Useful links
+              </h6>
+              <p className="mb-4">
+                <a href="#!">Home</a>
+              </p>
+              <p className="mb-4">
+                <a href="#!">About</a>
+              </p>
+              <p className="mb-4">
+                <a href="#!">Events</a>
+              </p>
+              <p>
+                <a href="#!">Venues</a>
+              </p>
+            </div>
+            {/* <!-- Contact section --> */}
+            <div>
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start text-[#fd614a]">
+                Contact
+              </h6>
+              <p className="mb-4 flex items-center gap-2 justify-center md:justify-start">
+                <FaLocationDot />
+                Dhaka, Bangladesh
+              </p>
+              <p className="mb-4 flex items-center gap-2 justify-center md:justify-start">
+                <MdEmail />
+                info@example.com
+              </p>
+              <p className="mb-4 flex items-center gap-2 justify-center md:justify-start">
+                <FaPhoneAlt />+ 01 234 567 88
+              </p>
+              <p className="flex items-center gap-2 justify-center md:justify-start">
+                <FaPhoneAlt />+ 01 234 567 89
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div className="px-5 py-4 text-white col-span-4">
-          <h5 className="text-uppercase mb-3 text-2xl font-bold font-mono">
-            Address
-          </h5>
-
-          <p className="text-base mb-8">
-            127/10 - Dhaka <br /> Bangladesh
-          </p>
-
-          <h6 className="font-bold mb-2">Follow us-</h6>
-          <hr className="mb-2" />
-          <a href="https://twitter.com/?lang=en">Twitter</a>
-
-          <a className=" mx-4" href="https://www.facebook.com/">
-            Facebook
-          </a>
-
-          <a href="https://www.instagram.com/">Instagram</a>
-
-          <a className=" mx-4" href="https://www.skype.com/en/">
-            Skype
-          </a>
+        {/* <!--Copyright section--> */}
+        <div className="bg-[#ff9382] p-6 text-center dark:bg-neutral-700">
+          {/* <span>© 2023 Copyright:</span> */}
+          Copyright &copy;2024 All Rights Reserved | This Website is made with
+          &#x003C;&#10083;&#x003E; by Afsana Meem
         </div>
-      </div>
-      <hr />
-
-      <p className="text-center mt-4 pb-4 text-white">
-        Copyright &copy;2024 All Rights Reserved | This Website is made with
-        &#x003C;&#10083;&#x003E; by Afsana Meem{" "}
-      </p>
+      </footer>
     </div>
   );
 };
